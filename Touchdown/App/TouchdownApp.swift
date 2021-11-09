ed349f4cd68078a8c4d7e6e29d20a3544a837e8b
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-09 14:50:32 +0100

    initial commit

---
+++ Touchdown/App/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Piotr Szerszeń on 09/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
